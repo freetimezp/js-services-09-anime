fafad09f6c4ae89776493075858667809b3503f6
freetimezp <kraftukrnet@ukr.net>
2023-04-29 16:49:41 +0300

    04. js-services-09-anime. Brand section. Swiper slider.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,26 @@
 /*=============== SCROLL HEADER ===============*/
 
 /*=============== BRAND SWIPER ===============*/
-
+var brandSwiper = new Swiper('.brand-swiper', {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+    }
+});
 /*=============== ANIME SWIPER ===============*/
